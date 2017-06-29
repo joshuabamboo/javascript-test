202d024f3bba56ed294989a2c1131361b4c412a4
jprosario <joshuaowens011@gmail.com>
2017-06-29 10:01:50 -0400

    regex 6/6

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,27 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return str.match(/\d/) ? true : false
   },
 
   containsRepeatingLetter: function(str) {
-
+    return str.match(/(\D)\1/) ? true : false
   },
 
   endsWithVowel: function(str) {
-
+    return str.match(/[aeiouAEIOU]$/) ? true : false
   },
 
   captureThreeNumbers: function(str) {
-
+    let match = str.match(/\d{3}/)
+    return str.match(/\d{3}/) ? match[0] : false
   },
 
   matchesPattern: function(str) {
-
+    return str.match(/^\d{3}-\d{3}-\d{4}$/) ? true : false
   },
 
   isUSD: function(str) {
-
+    return str.match(/^\$[0-9]{1,3}(,[0-9]{3})*(\.[0-9]{2})?$/) ? true : false
   }
 };
