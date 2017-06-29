fe9daa3a39bd7bd489e83166631829146546267d
jprosario <joshuaowens011@gmail.com>
2017-06-29 10:02:14 -0400

    strings 2/3

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    let previousChar
+    let uniqueChars = []
+    for (var i = 0; i < str.length; i++) {
+      if (previousChar != str[i]) {
+        charCount = str.match(new RegExp(str[i], "g")).length
+        for (var j = 0; j < amount && j < charCount; j++) {
+          uniqueChars.push(str[i])
+        }
+      }
+      previousChar = str[i]
+    }
+    return uniqueChars.join('')
   },
 
   wordWrap: function(str, cols) {
@@ -10,6 +21,10 @@ exports.stringsAnswers = {
   },
 
   reverseString: function(str) {
-
+    let reversedStr = ''
+    for (var i = str.length - 1; i >= 0; i--) {
+      reversedStr += str[i] 
+    }
+    return reversedStr
   }
 };
