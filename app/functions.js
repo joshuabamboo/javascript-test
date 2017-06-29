8f8a6da2c5bef58c8141a85e3df0f606da44366e
jprosario <joshuaowens011@gmail.com>
2017-06-29 10:00:11 -0400

    functions 4/9

--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+    return fn(...arr)
   },
 
   speak: function(fn, obj) {
-
+    return fn.apply(obj)
   },
 
-  functionFunction: function(str) {
-
+  functionFunction: function(strBegin) {
+    return function(strEnd){
+      return `${strBegin}, ${strEnd}`
+    }
   },
 
   makeClosures: function(arr, fn) {
-
+    fn(arr)
+    return arr
   },
 
   partial: function(fn, str1, str2) {
@@ -22,11 +25,13 @@ exports.functionsAnswers = {
   },
 
   useArguments: function() {
-
+    let args = Array.from(arguments);
+    return args.reduce((acc, n) => acc + n)
   },
 
   callIt: function(fn) {
-
+    let args = Array.from(arguments);
+    return fn(...args)
   },
 
   partialUsingArguments: function(fn) {
