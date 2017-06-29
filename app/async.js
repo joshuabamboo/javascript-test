a94ee5c436f4cd2b1879fe4458d92d27a3366da6
jprosario <joshuaowens011@gmail.com>
2017-06-29 09:58:08 -0400

    async 1/2

--- app/async.js
+++ app/async.js
@@ -2,10 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    
   },
 
   manipulateRemoteData: function(url) {
-
+    return $.getJSON(url)
+    
+    .then(function(data) {
+      let names = []
+      for(var key in data){
+        data[key].map(function(obj){
+          let name = Object.keys(obj)[0]
+          names.push(obj[name])
+        })
+      }
+      return names.sort()
+    })
   }
 };
